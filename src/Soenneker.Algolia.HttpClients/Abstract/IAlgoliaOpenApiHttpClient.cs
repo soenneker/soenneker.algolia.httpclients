288046e1cd9fdd3e2a59e18bc68da9658dcb64f5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:56 -0500

    xml commenting

--- src/Soenneker.Algolia.HttpClients/Abstract/IAlgoliaOpenApiHttpClient.cs
+++ src/Soenneker.Algolia.HttpClients/Abstract/IAlgoliaOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Algolia.HttpClients.Abstract;
 /// </summary>
 public interface IAlgoliaOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
